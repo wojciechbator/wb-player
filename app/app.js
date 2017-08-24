118334abca806009ade270364172c18f04dfe8d1
wojciech.bator <wojciech.jan.bator@gmail.com>
2017-08-24 16:37:23 +0200

    Added some basic react stuff, gonna remove audio streaming from node and move it to web audio api

--- app/app.js
+++ app/app.js
@@ -1,10 +1,32 @@
 const cottage = require('cottage');
+const webpack = require('webpack');
+const { devMiddleware, hotMiddleware } = require('koa-webpack-middleware');
 const { createReadStream } = require('fs');
 const path = require('path');
 const streamData = require('./audio-stream/index');
+const webpackConfig = require('./webpack.config');
+
 const app = cottage();
+const compiler = webpack(webpackConfig);
 
 app.use(streamData());
+app.use(devMiddleware(compiler, {
+    noInfo: false,
+    quiet: false,
+    lazy: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: true
+    },
+    publicPath: path.resolve(__dirname, 'dist'),
+    headers: { 'X-Custom-Header': 'yes' },
+    stats: {
+        colors: true
+    }
+}));
+app.use(hotMiddleware(compiler, {
+
+}));
 
 app.get('/', async (req, res) => {
     res.type = 'html';
