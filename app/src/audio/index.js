f20e159cfbf9091a76aed2e45fc1d6b1f4d99cb9
wojciech.bator <wojciech.jan.bator@gmail.com>
2017-08-27 12:57:12 +0200

    Fixed overcomplicated audioContext initialization

--- app/src/audio/index.js
+++ app/src/audio/index.js
@@ -1,16 +1,7 @@
 export default class AudioStream {
     constructor() {
-        const contextClass = (window.AudioContext ||
-            window.webkitAudioContext ||
-            window.mozAudioContext ||
-            window.oAudioContext ||
-            window.msAudioContext);
-        if (contextClass) {
-            const context = new contextClass();
-        }
-        else
-            throw new Error('Web Audio API is not available, please use supported browser');
-
+        const contextClass = window.AudioContext;
+        const context = new contextClass();
         const source = context.createBufferSource();
         const gain = context.createGain();
         source.connect(gain);
