a052fa49c4e9d05b316c086080b5dba1ab1b3b53
wojciech.bator <wojciech.jan.bator@gmail.com>
2017-08-30 13:52:26 +0200

    Working audio and one simple dial

--- client/audio/index.js
+++ client/audio/index.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 
+let BUFF_SIZE_RENDERER = 16384,
+    audioInput = null,
+    inputStream = null,
+    gainNode = null,
+    scriptProcessorNode = null,
+    scriptProcessorAnalysisNode = null,
+    analyserNode = null
+
 export default class Audio extends Component {
     constructor(props) {
         super(props);
         const state = {
-            BUFF_SIZE_RENDERER: 16384,
-            audioInput: null,
-            inputStream: null,
-            gainNode: null,
-            scriptProcessorNode: null,
-            scriptProcessorAnalysisNode: null,
-            analyserNode: null
+            currentVolume: 0
         }
         this.captureAudio();
     }
@@ -36,38 +38,45 @@ export default class Audio extends Component {
     }
 
     startInput(audioContext, stream) {
-        // Improve this mess ffs
-        this.setState({ gainNode: audioContext.createGain() });
-        this.state.gainNode.connect(audioContext.destination);
-        this.setState({ inputStream: audioContext.createMediaStreamSource(stream) });
-        this.state.inputStream.connect(this.state.gainNode);
-        this.setState({ scriptProcessorNode: audioContext.createScriptProcessor(this.state.BUFF_SIZE_RENDERER, 1, 1) })
-        this.state.scriptProcessorNode.onaudioprocess = this.processInputBuffer;
-        this.state.inputStream.connect(this.state.scriptProcessorNode);
-        this.setState({ scriptProcessorAnalysisNode: audioContext.createScriptProcessor(2048, 1, 1) });
-        this.state.scriptProcessorAnalysisNode.connect(this.state.gainNode);
-        this.setState({ analyserNode: audioContext.createAnalyser() });
-        this.state.analyserNode.smoothingTimeConstant = 0;
-        this.state.analyserNode.fftSize = 2048;
-        this.state.inputStream.connect(this.state.analyserNode);
-        this.state.analyserNode.connect(this.state.scriptProcessorAnalysisNode);
-        const bufferLength = this.state.analyserNode.frequencyBinCount;
+        gainNode = audioContext.createGain();
+        gainNode.connect(audioContext.destination);
+        inputStream = audioContext.createMediaStreamSource(stream);
+        inputStream.connect(gainNode);
+        scriptProcessorNode = audioContext.createScriptProcessor(BUFF_SIZE_RENDERER, 1, 1);
+        scriptProcessorNode.onaudioprocess = this.processInputBuffer;
+        inputStream.connect(scriptProcessorNode);
+        scriptProcessorAnalysisNode = audioContext.createScriptProcessor(2048, 1, 1);
+        scriptProcessorAnalysisNode.connect(gainNode);
+        analyserNode = audioContext.createAnalyser();
+        analyserNode.smoothingTimeConstant = 0;
+        analyserNode.fftSize = 2048;
+        inputStream.connect(analyserNode);
+        analyserNode.connect(scriptProcessorAnalysisNode);
+        const bufferLength = analyserNode.frequencyBinCount;
         const arrayFreqDomain = new Uint8Array(bufferLength);
         const arrayTimeDomain = new Uint8Array(bufferLength);
 
-        console.log("bufferLength " + bufferLength);
 
-        this.state.scriptProcessorAnalysisNode.onaudioprocess = () => {
+        scriptProcessorAnalysisNode.onaudioprocess = () => {
 
-            this.state.analyserNode.getByteFrequencyData(arrayFreqDomain);
-            this.state.analyserNode.getByteTimeDomainData(arrayTimeDomain);
+            analyserNode.getByteFrequencyData(arrayFreqDomain);
+            analyserNode.getByteTimeDomainData(arrayTimeDomain);
 
-            if (this.state.inputStream.playbackState == this.state.inputStream.PLAYING_STATE) {
+            if (inputStream.playbackState == inputStream.PLAYING_STATE) {
 
                 this.showSomeData(arrayFreqDomain, 5, "frequency");
                 this.showSomeData(arrayTimeDomain, 5, "time");
             }
         };
+        this.changeVolume();
+    }
+
+    changeVolume() {
+        document.getElementById('volume').addEventListener('change', (event) => {
+            const currentVolume = event.target.value;
+            this.setState({ currentVolume });
+            gainNode.gain.value = this.state.currentVolume;
+        });
     }
 
     processInputBuffer(event) {
@@ -75,27 +84,21 @@ export default class Audio extends Component {
         inputOutputBuffer = event.inputBuffer.getChannelData(0);
     }
 
-    changeVolume(event) {
-        const currVolume = event.target.value;
-        this.setState({ gainNode: { gain: { value: currVolume } } });
-        console.log("currVolume ", currVolume);
-    }
-
     showSomeData(givenTypedArray, numRowToDisplay, label) {
 
         const sizeBuffer = givenTypedArray.length;
         let index = 0;
-        console.log("__________ " + label);
+        // console.log("__________ " + label);
         if (label === "time") {
             for (; index < numRowToDisplay && index < sizeBuffer; index += 1) {
 
                 let currValueTime = (givenTypedArray[index] / 128) - 1.0;
-                console.log(currValueTime);
+                // console.log(currValueTime);
             }
         } else if (label === "frequency") {
             for (; index < numRowToDisplay && index < sizeBuffer; index += 1) {
 
-                console.log(givenTypedArray[index]);
+                // console.log(givenTypedArray[index]);
             }
         } else {
             throw new Error("ERROR - must pass time or frequency");
@@ -106,7 +109,7 @@ export default class Audio extends Component {
         return (
             <div>
                 <p>Volume</p>
-                <input id="volume" type="range" min="0" max="1" step="0.1" value="0.5" onChange={this.changeVolume} />
+                <input id="volume" type="range" min="0" max="1" step="0.1" defaultValue="0.5" />
             </div>
         );
     }
