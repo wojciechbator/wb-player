a3dbb5b0a1a9600feb4205a7ae5e2fe62f388380
Wojciechbator <wojciech.jan.bator@gmail.com>
2017-09-07 14:02:28 +0200

    moved logic to action creators, works like a charm

--- client/components/audio/gain/index.js
+++ client/components/audio/gain/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Gain from './presentation';
-import { storeGainNodeValues, addNodeToChain } from '../../../redux/actions/audioActions';
+import { gainValuesCreator, addNodeCreator } from '../../../redux/actions/audioActions';
 import audioChain from '../../../utils/audioChain';
 
 class GainNode extends Component {
@@ -17,8 +18,8 @@ class GainNode extends Component {
     
     componentDidMount() {
         const gainNode = this.props.audioContext.createGain();
-        this.props.dispatch(storeGainNodeValues(gainNode));
-        this.props.dispatch(addNodeToChain(gainNode));
+        this.props.gainValuesCreator(gainNode);
+        this.props.addNodeCreator(gainNode);
         audioChain(this.props.currentChain[this.props.currentChain.indexOf(gainNode) - 1], gainNode, this.props.currentChain[this.props.currentChain.indexOf(gainNode) + 1], true, true, this.props.audioContext);
     }
     
@@ -44,4 +45,6 @@ const mapStateToProps = (store) => {
     }
 }
 
-export default connect(mapStateToProps)(GainNode);
+const mapDispatchToProps = (dispatch) => bindActionCreators({gainValuesCreator, addNodeCreator}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(GainNode);
--- client/redux/actions/audioActions.js
+++ client/redux/actions/audioActions.js
@@ -41,3 +41,39 @@ export const removeNodeFromChain = (node) => {
         node
     }
 }
+
+export const initContextCreator = (audioContext) => {
+    return dispatch => {
+        dispatch(initializeAudioContext(audioContext));
+    }
+}
+
+export const inputStreamCreator = (inputStream) => {
+    return dispatch => {
+        dispatch(storeInputStream(inputStream));
+    }
+}
+
+export const gainValuesCreator = (gainNode) => {
+    return dispatch => {
+        dispatch(storeGainNodeValues(gainNode));
+    }
+}
+
+export const filterValuesCreator = (filterNode) => {
+    return dispatch => {
+        dispatch(storeFilterNodeValues(filterNode));
+    }
+}
+
+export const addNodeCreator = (node) => {
+    return dispatch => {
+        dispatch(addNodeToChain(node));
+    }
+}
+
+export const removeNodeCreator = (node) => {
+    return dispatch => {
+        dispatch(removeNodeFromChain(node));
+    }
+}
