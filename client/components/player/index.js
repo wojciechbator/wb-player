76906d63a133ed50555574c1830defb68b85929f
Wojciech Bator <wojciech.jan.bator@gmail.com>
2017-10-07 19:23:06 +0200

    Initial audio player done!

--- client/components/player/index.js
+++ client/components/player/index.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 import Search from '../search';
+import {Button} from 'primereact/components/button/Button';
 
 import './player.css';
 
 export default class Player extends Component {
+    constructor(props) {
+        super(props);
+        this.playAudio = this.playAudio.bind(this);
+        this.pauseAudio = this.pauseAudio.bind(this);
+        this.loadAudio = this.loadAudio.bind(this);
+    }
+
+    playAudio() {
+        const audioPlayer = document.getElementById('audio_player');
+        audioPlayer.play();
+    }
+    
+    pauseAudio() {
+        const audioPlayer = document.getElementById('audio_player');
+        audioPlayer.pause();
+    }
+
+    loadAudio() {
+        const audioFile = document.getElementById('audio_file');
+        const audioPlayer = document.getElementById('audio_player');
+        const file = URL.createObjectURL(audioFile.files[0]);        
+        audioPlayer.src = file;
+    }
+
     render() {
         return (
             <div className="player-module">
                 <Search />
+                <label for="audio_file" className="file-upload">
+                    UPLOAD AUDIO
+                    <input id="audio_file" className="ui-button" type="file" onChange={this.loadAudio} accept="audio/*" />
+                </label>
+                <Button label="PLAY" onClick={this.playAudio} />
+                <Button label="STOP" onClick={this.pauseAudio} />
+                <audio id="audio_player" />
             </div>
         );
     }
