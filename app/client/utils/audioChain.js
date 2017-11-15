0cb80a03ec93ea640b23c8b032be025cef7ad122
Wojciech Bator <wojciech.bator@sienn.pl>
2017-11-15 20:04:46 +0100

    working audio chain

--- app/client/utils/audioChain.js
+++ app/client/utils/audioChain.js
@@ -14,6 +14,18 @@ class AudioChain extends Component {
         }
     }
 
+    componentDidUpdate() {
+        this.props.inputStream.connect(this.props.currentChain[0]);
+        for (let i = 0; i < this.props.currentChain.length; i++) {
+            if (this.props.currentChain[i + 1])
+                this.props.currentChain[i].connect(this.props.currentChain[i + 1]);
+            else {
+                this.props.currentChain[i].connect(this.props.analyserNode);
+                this.props.analyserNode.connect(this.props.audioContext.destination);
+            }
+        }
+    }
+
     render() {
         return null;
     }
